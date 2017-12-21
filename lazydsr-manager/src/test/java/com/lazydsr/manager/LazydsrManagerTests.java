978a7727d043f2469ddb9bab9bdf0848a308ccec
lazydsr <168367406@qq.com>
2017-12-21 20:30:08 +0800

    Initial commit

---
+++ lazydsr-manager/src/test/java/com/lazydsr/manager/LazydsrManagerTests.java
@@ -0,0 +1,16 @@
+package com.lazydsr.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LazydsrManagerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
