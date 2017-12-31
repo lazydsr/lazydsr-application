3ed69dc54d7bc5298c53fbe62cf91e468d08cc1c
lazydsr <168367406@qq.com>
2017-12-31 23:23:00 +0800

    添加多配置文件

--- lazydsr-manager/src/main/java/com/lazydsr/manager/config/MvcConfiguration.java
+++ lazydsr-manager/src/main/java/com/lazydsr/manager/config/MvcConfiguration.java
@@ -1,6 +1,11 @@
 package com.lazydsr.manager.config;
 
+import com.lazydsr.manager.interceptor.UserInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,10 +18,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Info: 给静态资源添加额外的路径
  */
 @Configuration
+//@PropertySource("classpath:config/application-interceptor.yml")
+@PropertySource("classpath:config/interceptor.properties")
+@ConfigurationProperties
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
+    @Value("${status}")
+    private boolean userStatus;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        if (userStatus) {
+            registry.addInterceptor(new UserInterceptor()).addPathPatterns("/**").excludePathPatterns("/error", "/login");
+        }
     }
 }
---
+++ lazydsr-manager/src/main/java/com/lazydsr/manager/config/bean/UserInterceptorConfigBean.java
@@ -0,0 +1,18 @@
+package com.lazydsr.manager.config.bean;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * UserInterceptorConfigBean
+ * PROJECT_NAME: lazydsr-application
+ * PACKAGE_NAME: com.lazydsr.manager.config.bean
+ * Created by Lazy on 2017/12/31 22:33
+ * Version: 0.1
+ * Info: @TODO:...
+ */
+@Component
+@PropertySource("")
+public class UserInterceptorConfigBean {
+
+}
